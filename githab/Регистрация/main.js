7801b4cdba222c440621c79bc2db31235d4f5ac0
yuusf991 <63147524+yuusf991@users.noreply.github.com>
2020-05-20 17:55:36 +0600

    my projects

---
+++ githab/Регистрация/main.js
@@ -0,0 +1,54 @@
+// function transferNumber() {
+
+//   var postNumberPhone = document.getElementsByClassName("card-block__form");
+//   var postValue = postNumberPhone[0].value;
+//   var getNumberPhone = document.getElementsByClassName('form');
+//   var getValue = getNumberPhone[0].value;
+//   getValue += postValue;
+
+//   console.log(postValue);
+//   console.log(getValue);
+// }
+
+
+
+// var className = document.getElementsByClassName('myClass');
+
+
+	  function say_hi() {
+
+    var fname = document.getElementsByClassName('card-block__form');
+    var fname_from = fname [0].value;
+    var lname = document.getElementsByClassName('form');
+    var lname_from = lname [0].value;
+
+    var aname = document.getElementsByClassName('form_age');
+    var aname_from = aname [0].value;
+    var pname = document.getElementsByClassName('form_Phone');
+    var pname_from = pname [0].value;
+
+    console.log(fname_from);
+	console.log(lname_from);
+    
+    console.log(aname_from);
+	console.log(pname_from);
+}
+
+
+ 
+// document.getElementById('say').addEventListener('click', say_hi);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
